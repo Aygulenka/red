59dea357997e858907cfd511df16d69e3385867c
Aygul Hurbanova <aygulhurbanova@gmail.com>
2023-10-20 10:06:53 +0400

    first commit

---
+++ makRedev-maketRedev/script.js
@@ -0,0 +1,52 @@
+function myFunction() {
+  let x = document.getElementById("myDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function kartochkaClick() {
+  let x = document.getElementById("kartochka-click");
+  let we = document.getElementById("spec2")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    we.style.display = "block"
+  } else {
+    x.style.display = "none"
+    we.style.display = "none"
+  }
+}
+function kartochkaClick1() {
+  let x = document.getElementById("kartochka1-click");
+  let we = document.getElementById("spec2")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    we.style.display = "block"
+  } else {
+    x.style.display = "none"
+   we.style.display = "none"
+  }
+}
+function kartochkaClick2() {
+  let x = document.getElementById("kartochka2-click");
+  let we = document.getElementById("spec2")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    we.style.display = "block"
+  } else {
+    x.style.display = "none"
+   we.style.display = "none"
+  }
+}
+function kartochkaClick3() {
+  let x = document.getElementById("kartochka3-click");
+  let we = document.getElementById("spec2")
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    we.style.display = "block"
+  } else {
+    x.style.display = "none"
+   we.style.display = "none"
+  }
+}
